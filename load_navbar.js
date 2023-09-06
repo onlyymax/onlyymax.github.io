5833c7ba3d9cfde7fe1372c018427301eb80a9eb
OnlyMax <maxlomont99@gmail.com>
2023-09-07 00:44:23 +0200

    Commit sus?

--- load_navbar.js
+++ load_navbar.js
@@ -1,5 +1,29 @@
-fetch('/layout/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+script.onload = function () {
+    fetch('/layout/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        });
+
+    $(document).ready(function () {
+        var prevScrollPos = $(window).scrollTop();
+
+        $(window).scroll(function () {
+            var currentScrollPos = $(window).scrollTop();
+            var navbarHeight = $('.navbar').outerHeight();
+
+            if (prevScrollPos > currentScrollPos) {
+                $('.navbar').css('top', '0');
+            } else {
+                $('.navbar').css('top', '-' + navbarHeight + 'px');
+            }
+
+            prevScrollPos = currentScrollPos;
+        });
     });
+};
+
+document.head.appendChild(script);
