2fa1236f99b42df258f0f6a978a1812b9a713b26
OnlyMax <maxlomont99@gmail.com>
2023-09-14 23:02:43 +0200

    Pollok

--- load_layoyt.js
+++ load_layoyt.js
@@ -1,8 +1,38 @@
 const fetchAndInject = async (url, elementId) => {
-    const response = await fetch(url);
-    const data = await response.text();
-    document.getElementById(elementId).innerHTML = data;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+        }
+        const data = await response.text();
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.innerHTML = data;
+        } else {
+            throw new Error(`Element with ID '${elementId}' not found`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-fetchAndInject('/layout/footer.html', 'footer-placeholder');
-fetchAndInject('/layout/navbar.html', 'navbar-placeholder');
+const loadContent = async () => {
+    await fetchAndInject('/layout/footer.html', 'footer-placeholder');
+    await fetchAndInject('/layout/navbar.html', 'navbar-placeholder');
+};
+
+window.addEventListener('DOMContentLoaded', loadContent);
+
+var prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', function () {
+    var currentScrollPos = window.pageYOffset;
+    var navbar = document.querySelector('.navbar');
+    if (navbar) {
+        if (prevScrollpos > currentScrollPos) {
+            navbar.style.top = "15px";
+        } else {
+            navbar.style.top = "-60px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+});
