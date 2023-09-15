f4b1303b1b7c6eaf1fe558be5a1b242c5145b7d5
OnlyMax <maxlomont99@gmail.com>
2023-09-15 20:51:07 +0200

    Test

--- load_layoyt.js
+++ load_layoyt.js
@@ -1,36 +1,23 @@
 const fetchAndInject = async (url, elementId) => {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
 
-        const data = await response.text();
-        const element = document.getElementById(elementId);
-        if (!element) throw new Error(`Element with ID '${elementId}' not found`);
+    const data = await response.text();
+    const element = document.getElementById(elementId);
+    if (!element) throw new Error(`Element with ID '${elementId}' not found`);
 
-        element.innerHTML = data;
-    } catch (error) {
-        console.error(error);
-    }
+    element.innerHTML = data;
 };
 
 const loadContent = async () => {
     await fetchAndInject('/layout/footer.html', 'footer-placeholder');
     await fetchAndInject('/layout/navbar.html', 'navbar-placeholder');
-
-    const currentPath = window.location.pathname;
-    const navbarLinks = document.querySelectorAll(".navbar a");
-
-    navbarLinks.forEach((link) => {
-        if (link.getAttribute("href") === currentPath) {
-            link.classList.add("active");
-        }
-    });
 };
 
 const handleScroll = () => {
     const currentScrollPos = window.scrollY;
     const navbar = document.querySelector('.navbar');
-    if (navbar) navbar.style.top = (prevScrollPos > currentScrollPos) ? "15px" : "-60px";
+    navbar.style.top = (prevScrollPos > currentScrollPos) ? "15px" : "-60px";
     prevScrollPos = currentScrollPos;
 };
 
